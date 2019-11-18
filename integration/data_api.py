1284e37ba76b8fc6e17a9996681067365d767b5b
xonnel <xonneln@gmail.com>
2019-11-18 20:26:39 +0000

    NEW: Added snapshot functionality which allows saved snaps.

--- integration/data_api.py
+++ integration/data_api.py
@@ -47,42 +47,62 @@ class PlandayData(object):
     def get_headers(self):
         return {'X-ClientId': self.client_id, 'Authorization': 'Bearer {0}'.format(self.access_token)}
 
-    def get_by_dates(self, dates, enhance=True):
+    def get_by_dates(self, dates, snapshots=None, retrieve_historical=False, enhance=True):
         shifts = {}
 
         for branch, branch_id in self.branches.items():
-            shifts[branch] = self.get_by_dates_branch(dates, branch_id, enhance=enhance)
+            branch_snapshot = snapshots.get(branch) if isinstance(snapshots, dict) else None
+            shifts[branch] = self.get_by_dates_branch(dates,
+                                                      branch_id,
+                                                      branch_snapshot=branch_snapshot,
+                                                      retrieve_historical=retrieve_historical,
+                                                      enhance=enhance)
 
         return shifts
 
-    def get_by_dates_branch(self, dates, branch_id, enhance=True):
-        shifts = {}
+    def get_by_dates_branch(self, dates, branch_id, branch_snapshot=None, retrieve_historical=False, enhance=True):
+        if isinstance(branch_snapshot, dict):
+            shifts = {date: data for date, data in branch_snapshot.items() if date in dates}
+        else:
+            shifts = {}
+
+        def _enhance(shift_data):
+            for data in shift_data:
+                data['startDateTime'] = datetime.datetime.strptime(data['startDateTime'], '%Y-%m-%dT%H:%M')
+                data['endDateTime'] = datetime.datetime.strptime(data['endDateTime'], '%Y-%m-%dT%H:%M')
+                data['shiftTime'] = data['endDateTime'] - data['startDateTime']
+
+            return shift_data
 
         for date in dates:
-            date_str = '{:%Y-%m-%d}'.format(date)
+            if not retrieve_historical and shifts:
+                if max(shifts) >= date:
+                    if enhance:
+                        shift_data = shifts[date]
+                        shifts[date] = _enhance(shift_data)
+                    continue
 
-            url = '{url}?departmentId={branch_id}&from={from_date}&to={to_date}'.format(url=self.punch_clock_url,
-                                                                                        branch_id=branch_id,
-                                                                                        from_date=date_str,
-                                                                                        to_date=date_str)
+            if date not in shifts:
+                date_str = '{:%Y-%m-%d}'.format(date)
+                url = '{url}?departmentId={branch_id}&from={from_date}&to={to_date}'.format(url=self.punch_clock_url,
+                                                                                            branch_id=branch_id,
+                                                                                            from_date=date_str,
+                                                                                            to_date=date_str)
 
-            # Dirty hack
-            for attempt in range(self.attempts):
-                response = requests.get(url, headers=self.get_headers())
-                if response.status_code == 200:
-                    break
-                else:
-                    logging.info('[shifts_by_branch] status_code={0}'.format(response.status_code))
-                    self.refresh_access_token()
+                # Dirty hack
+                for attempt in range(self.attempts):
+                    response = requests.get(url, headers=self.get_headers())
+                    if response.status_code == 200:
+                        break
+                    else:
+                        logging.info('[shifts_by_branch] status_code={0}'.format(response.status_code))
+                        self.refresh_access_token()
 
-            shift_data = response.json()['data']
+                shift_data = response.json()['data']
 
-            if enhance:
-                for data in shift_data:
-                    data['startDateTime'] = datetime.datetime.strptime(data['startDateTime'], '%Y-%m-%dT%H:%M')
-                    data['endDateTime'] = datetime.datetime.strptime(data['endDateTime'], '%Y-%m-%dT%H:%M')
-                    data['shiftTime'] = data['endDateTime'] - data['startDateTime']
+                if enhance:
+                    shift_data = _enhance(shift_data)
 
-            shifts[date] = shift_data
+                shifts[date] = shift_data
 
         return shifts
